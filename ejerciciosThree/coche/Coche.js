d02b9fcf1bf2a76a2ee22e70710130f3c1228013
donBravo1011 <130547772+donBravo1011@users.noreply.github.com>
2024-04-29 11:21:06 +0200

    Version coche
    a

---
+++ ejerciciosThree/coche/Coche.js
@@ -0,0 +1,53 @@
+
+import * as THREE from 'three'
+import { CSG } from '../libs/CSG-v2.js'
+import { MTLLoader } from '../libs/MTLLoader.js'
+import { OBJLoader } from '../libs/OBJLoader.js'
+
+class Coche extends THREE.Object3D {
+  constructor(gui,titleGui) {
+    super();
+    
+    var materialLoader = new MTLLoader();
+    var objectLoader = new OBJLoader();
+    materialLoader . load('../modelo_cargado/model.mtl' ,
+    ( materials ) => {
+    objectLoader.setMaterials(materials);
+    objectLoader . load('../modelo_cargado/model.obj' ,
+    ( object ) => {
+    this . add ( object ) ;
+    } , null , null ) ;
+    } ) ;
+
+  }
+  
+  
+
+  
+  createGUI (gui,titleGui) {
+    // Controles para el movimiento de la parte móvil
+    this.guiControls = {
+      rotacion : 0
+    } 
+    
+    // Se crea una sección para los controles de la caja
+    var folder = gui.addFolder (titleGui);
+    // Estas lineas son las que añaden los componentes de la interfaz
+    // Las tres cifras indican un valor mínimo, un máximo y el incremento
+    folder.add (this.guiControls, 'rotacion', -0.125, 0.2, 0.001)
+      .name ('Apertura : ')
+      .onChange ( (value) => this.setAngulo (-value) );
+  }
+  
+  
+  
+  setAngulo (valor) {
+    this.movil.rotation.z = valor;
+  }
+  
+  update () {
+    // No hay nada que actualizar ya que la apertura de la grapadora se ha actualizado desde la interfaz
+  }
+}
+
+export { Coche }
